97b6d7ea6401750d0919c651741d6c750156c8fd
amalsherifsalem <amalsherifsalem@gmail.com>
2023-02-25 21:07:20 -0800

    betty coding style error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-
+/*
+ *
+ * Main 
+ * return always 0 =success
+ * print without printf
+ */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
