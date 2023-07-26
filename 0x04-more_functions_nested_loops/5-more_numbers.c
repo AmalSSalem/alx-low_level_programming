9b8e930b98f9ec0ecd755692227466cdbad08108
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-26 07:05:43 -0700

    0x04: tasks from 3 to 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times
+ * the numbers, from 0 to 14
+ * followed by new line
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0;j < 10;j++)
+	{
+		for(i = 0;i < 15;i++)
+		{
+			_putchar(i+'0')
+		}
+		_putchar('\n')
+	}
+}
