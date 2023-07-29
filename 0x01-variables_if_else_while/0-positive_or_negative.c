d752c6ce0eac22c37238c0fdea417913b6853c41
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-29 05:11:00 -0700

    fix logical error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-		printf("is zero");
+		printf(n, "is zero\n");
 	else if (n > 0)
-		printf("is positive");
+		printf(n, "is positive\n");
 	else if (n < 0)
-		printf("is negative");
+		printf(n, "is negative\n");
 	return (0);
 }
