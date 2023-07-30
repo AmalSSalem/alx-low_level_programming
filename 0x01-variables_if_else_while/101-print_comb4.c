eb7f322e9230141edde7e45d1d37a38a8c48749a
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-30 01:14:07 -0700

    runtime error fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k <= '9';k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
 				putchar(j);
