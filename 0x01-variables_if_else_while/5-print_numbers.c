b1aacbb838d6c013a3f35541b4ada260a2a4ea56
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-29 22:03:53 -0700

    submit task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - code is inside it
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
