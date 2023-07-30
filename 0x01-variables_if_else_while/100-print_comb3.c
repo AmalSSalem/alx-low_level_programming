c4ddd9cf7f922637747b57adabdcb2a2707c8317
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-30 00:37:51 -0700

    task 10 submit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - code is inside it
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		for (j = i; j <= '9'; j++)
+		{
+			putchar(j);
+			if (i != '9' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
