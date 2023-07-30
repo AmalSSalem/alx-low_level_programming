d5c978611c0aeeae23d0e4a0394aed4d150742d2
AmalSSalem <amalsherifsalem@gmail.com>
2023-07-29 22:58:34 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - code should be here
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
