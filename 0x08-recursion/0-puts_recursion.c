c75ea3f122c99da53eaad5aa8a08895f389c550f
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-02 11:59:04 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string in reverse
+ * @s: string to reverse
+ */
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+		_puts_recursion(*(s+1));
+	_putchar(s);
+}
