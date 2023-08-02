d8cdd1d9884229f3c5fcfc77637050057e3b1b76
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-02 12:57:08 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - get length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	i = 0 ;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion((s + 1));
+		i++;
+	}
+	return (i);
+	
