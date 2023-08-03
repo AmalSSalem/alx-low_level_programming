cdfd74016a4f9920c1b8633d0cff793bd0d6a06c
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-03 15:24:33 -0700

    submit all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return actual_sqrt(n, 0);
+	return (actual_sqrt(n, 0));
 }
 /**
  * actual_sqrt - check if i is sqrt of n
@@ -20,7 +20,7 @@ int _sqrt_recursion(int n)
 int actual_sqrt(int n, int i)
 {
 	if (i * i > n)
-		return(-1);
+		return (-1);
 	else if (i * i == n)
 		return (i);
 	return (actual_sqrt(n, i + 1));
