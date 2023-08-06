b23793009f2e463fc1f65239d7e0873ae5fef109
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-06 14:07:00 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - your code is written here
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
