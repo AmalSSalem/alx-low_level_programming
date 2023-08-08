272be5e37dc8264fdecce3df4c02c8244fe91e56
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-07 17:48:05 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to copy
+ * Return: a pointer to a newly allocated space
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	array = malloc(sizeof(str));
+	i = (sizeof(array) / sizeof(int));
+	while (i >= 0)
+	{
+		array[i] = str[i];
+		i--;
+	}
+	return (array);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,2 @@
 0-create_array.c: 0-create_array.c
+1-strdup.c: Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
