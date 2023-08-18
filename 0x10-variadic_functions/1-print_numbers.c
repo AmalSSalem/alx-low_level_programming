8c0b65ddc736a71945aeda47eefdb6acfc08f233
AmalSSalem <amalsherifsalem@gmail.com>
2023-08-17 19:22:04 -0700

    fix error logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		if (!separator || i == 1)
+		if (!separator || i == 0)
 			printf("%d", va_arg(list, int));
 		else
 			printf("%s%d", separator, va_arg(list, int));
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,10 +13,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
+		string = va_arg(list, char);
 		if (!separator || i == 0)
-			printf("%s", va_arg(list, char));
-		else
-			printf("%s%s", separator, va_arg(list, char));
+		{	printf("%s", string);
+			continue;
+		}
+		else if (string == NULL)
+			string = "(nil)";
+
+		printf("%s%s", separator, string);
 	}
 	va_end(list);
 }
