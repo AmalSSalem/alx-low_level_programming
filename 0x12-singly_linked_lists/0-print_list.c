50987ea18774789dd014ec88ff6d22c573717f74
amalsherifsalem <amalsherifsalem@gmail.com>
2023-08-28 08:16:32 +0300

    logical error fix h was passed by reference so i needed to create a pointer to keep the header value and not lose the beginning of the list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,18 @@
 size_t print_list(const list_t *h)
 { 
 	size_t nodesNum;
+	list_t *pointer;
+	pointer = malloc(sizeof(list_t));
 
+	*pointer = *h;
 	nodesNum = 0;
-	while (h != NULL)
+	while (pointer != NULL)
 	{
-		if (h->str == NULL)
+		if (pointer->str == NULL)
 			printf ("%s\n","[0] (nil)");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", pointer->len, pointer->str);
+		pointer = pointer->next;
 		nodesNum++;
 	}
 	return (nodesNum);
