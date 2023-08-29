9a563a9e0f01d1c23f825023b09e47322c440f0c
amalsherifsalem <amalsherifsalem@gmail.com>
2023-08-29 05:23:18 +0300

    add: task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - add new node at the beginning of the list
+ * @head: the head of the list
+ * @str: string value it should had
+ * Return: the address of the new element,
+ * or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->next = (*head);
+	newNode->len = strlen(str);
+	newNode->str = malloc(newNode->len + 1);
+	strcpy(newNode->str, str);
+	*head = newNode;
+	return (newNode);
+
+}
